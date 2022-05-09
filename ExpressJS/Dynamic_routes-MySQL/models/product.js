0d18565620a72c2fc49c2123b017a3b425b12148
sanket <sanketdpr@gmail.com>
2022-05-09 13:00:42 +0530

    database created in MySQL and insert update delete select functionality included

---
+++ ExpressJS/Dynamic_routes-MySQL/models/product.js
@@ -0,0 +1,33 @@
+const db=require('../util/database')
+
+
+module.exports = class Product {
+  constructor(id,title, imageUrl, description, price) {
+    this.id=id;
+    this.title = title;
+    this.imageUrl = imageUrl;
+    this.description = description;
+    this.price = price;
+  }
+
+  save() {
+    if(this.id===null)
+    return db.execute('INSERT INTO products (title,price,description,imageUrl) VALUES (?,?,?,?)',
+                      [this.title,this.price,this.description,this.imageUrl])
+    else
+    return db.execute('UPDATE products SET title=?, price=?, description=?, imageUrl=? WHERE products.id=?',
+                      [this.title,this.price,this.description,this.imageUrl,this.id])
+  }
+
+  static deleteProductById(id){
+      return db.execute('DELETE FROM products WHERE products.id=?',[id])
+  }
+
+  static fetchAll() {
+    return db.execute('SELECT * FROM products')
+  }
+
+  static findById(id){
+    return db.execute('SELECT * FROM products WHERE products.id=?',[id])
+  }
+};
